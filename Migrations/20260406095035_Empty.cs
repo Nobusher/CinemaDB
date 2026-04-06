d9ce4e5ef47f07afa3eb5f67ea6bfaead48189f9
Paul Winter <zimin.pavel.v@gmail.com>
2026-04-06 13:34:58 +0300

    Renamed Ticket ->Tickets

---
+++ Migrations/20260406095035_Empty.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class Empty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ticket_Sessions_SessionId",
+                table: "Ticket");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Ticket",
+                table: "Ticket");
+
+            migrationBuilder.RenameTable(
+                name: "Ticket",
+                newName: "Tickets");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Ticket_SessionId_SeatNumber",
+                table: "Tickets",
+                newName: "IX_Tickets_SessionId_SeatNumber");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tickets",
+                table: "Tickets",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_Sessions_SessionId",
+                table: "Tickets",
+                column: "SessionId",
+                principalTable: "Sessions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_Sessions_SessionId",
+                table: "Tickets");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tickets",
+                table: "Tickets");
+
+            migrationBuilder.RenameTable(
+                name: "Tickets",
+                newName: "Ticket");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Tickets_SessionId_SeatNumber",
+                table: "Ticket",
+                newName: "IX_Ticket_SessionId_SeatNumber");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Ticket",
+                table: "Ticket",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ticket_Sessions_SessionId",
+                table: "Ticket",
+                column: "SessionId",
+                principalTable: "Sessions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
